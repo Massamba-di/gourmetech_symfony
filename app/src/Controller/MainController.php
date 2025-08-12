8b51ece703ba4b963af5aaa7530f75cb5a3a68a9
Massambadieng <thiebakon@gmail.com>
2025-08-13 00:37:44 +0200

    la page d'accueil styisé avec bootstrap

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -8,6 +8,7 @@ use App\Repository\CategoriesRepository;
 
 
 use App\Repository\DifficulteRepository;
+use App\Repository\RecettesRepository;
 use App\Repository\TempsDePreparationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,16 +17,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(CategoriesRepository $categoriesRepository, TempsDePreparationsRepository $tempsDePreparationsRepository,DifficulteRepository $difficulteRepository): Response
+    public function index(CategoriesRepository $categoriesRepository, TempsDePreparationsRepository $tempsDePreparationsRepository,DifficulteRepository $difficulteRepository, RecettesRepository $recettesRepository): Response
     {
         $categories = $categoriesRepository->findAll();
         $tempsDePreparations=$tempsDePreparationsRepository->findAll();
         $difficulte=$difficulteRepository->findAll();
+        $recettes=$recettesRepository->findAll();
 
         return $this->render('main/index.html.twig', [
             'categories' => $categories,
             'tempsDePreparations' => $tempsDePreparations,
             'difficulte' => $difficulte,
+            'recettes' => $recettes,
 
         ]);
     }
